433f238bab738a963c3ea30d9162c9e68c99c1fa
devahmadraza <devahmadraza296@gmail.com>
2026-08-05 23:51:08 +0500

    Trying to add API from Rapid Api

---
+++ script.js
@@ -0,0 +1,22 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+
+const options = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'YOUR_API_KEY',
+        'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com',
+        'Content-Type': 'application/json'
+    }
+};
+
+async function getWeather() {
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getWeather();
